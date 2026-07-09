48d9f69dc4f5d907d076d0a4faa164aa467be88a
orunbasar <aseraloginova@gmail.com>
2026-07-09 15:35:33 +0500

    Update update-products.js

--- scripts/update-products.js
+++ scripts/update-products.js
@@ -1,1 +1,18 @@
 
+const fs = require("fs");
+
+const products = [
+  {
+    title: "Тестовый товар",
+    category: "Тест",
+    store: "Admitad",
+    price: "19.99 USD",
+    image: "https://picsum.photos/600/400",
+    description: "Если вы видите этот товар, GitHub Actions работают.",
+    link: "https://google.com"
+  }
+];
+
+fs.writeFileSync("products.json", JSON.stringify(products, null, 2));
+
+console.log("products.json обновлен");
