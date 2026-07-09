cbe23a8a38dc2e21535e5845f5e4561561157962
orunbasar <aseraloginova@gmail.com>
2026-07-09 16:18:13 +0500

    Update update-products.js

--- scripts/update-products.js
+++ scripts/update-products.js
@@ -1,10 +1,17 @@
 const https = require("https");
 const csv = require("csv-parser");
 
-https.get(process.env.ADMITAD_FEED_URL, (res) => {
+const url = process.env.ADMITAD_FEED_URL;
+
+if (!url) {
+  throw new Error("ADMITAD_FEED_URL не задан");
+}
+
+https.get(url, (res) => {
   res
     .pipe(csv())
     .on("headers", (headers) => {
+      console.log("HEADERS:");
       console.log(headers);
       process.exit(0);
     });
