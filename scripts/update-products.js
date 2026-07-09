6c057d50e0262209845a17cbed0d58ff3af8a43b
orunbasar <aseraloginova@gmail.com>
2026-07-09 16:59:00 +0500

    Update update-products.js

--- scripts/update-products.js
+++ scripts/update-products.js
@@ -1,20 +1,53 @@
 const https = require("https");
+const csv = require("csv-parser");
+const fs = require("fs");
 
 const url = process.env.ADMITAD_FEED_URL;
 
+const products = [];
+
 https.get(url, (res) => {
-    let data = "";
 
-    res.on("data", chunk => {
-        data += chunk.toString();
+    const parser = csv();
 
-        if (data.length > 5000) {
-            console.log(data.substring(0, 5000));
-            res.destroy();   // сразу закрываем соединение
+    parser.on("data", (row) => {
+
+        const price = parseFloat(
+            String(row.sale_price || row.price)
+                .replace(/[^\d.]/g, "")
+        );
+
+        if (isNaN(price)) return;
+        if (price < 10 || price > 25) return;
+
+        products.push({
+            title: row.title,
+            description: row.description,
+            image: row.image_link,
+            link: row.link,
+            price: row.sale_price || row.price,
+            category: row.product_type || "AliExpress",
+            store: "AliExpress"
+        });
+
+        if (products.length >= 50) {
+            console.log("Нашли 50 товаров.");
+
+            fs.writeFileSync(
+                "products.json",
+                JSON.stringify(products, null, 2)
+            );
+
+            // ВАЖНО: сразу закрываем соединение
+            res.destroy();
+            parser.destroy();
         }
     });
 
-    res.on("close", () => {
-        process.exit(0);
+    parser.on("close", () => {
+        console.log("Готово.");
     });
+
+    res.pipe(parser);
+
 });
