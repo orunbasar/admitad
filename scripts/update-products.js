e1c388745ed19dcb880a39cddcf5afe741343e73
orunbasar <aseraloginova@gmail.com>
2026-07-10 11:57:57 +0500

    Update update-products.js

--- scripts/update-products.js
+++ scripts/update-products.js
@@ -1,42 +1,50 @@
 const https = require("https");
 const csv = require("csv-parser");
-const fs = require("fs");
 
 const url = process.env.ADMITAD_FEED_URL;
 
 console.log("Старт...");
-console.log(url.substring(0, 80) + "...");
+console.log(url);
 
 https.get(url, (res) => {
-
     console.log("HTTP:", res.statusCode);
     console.log("content-type:", res.headers["content-type"]);
     console.log("content-length:", res.headers["content-length"]);
 
-    let bytes = 0;
-
-    res.on("data", chunk => {
-        bytes += chunk.length;
-
-        if (bytes < 1024 * 1024) {
-            console.log("Получено", bytes, "байт");
-        }
-    });
-
     const parser = csv();
 
-    parser.on("headers", headers => {
+    let first = true;
+
+    parser.on("headers", (headers) => {
         console.log("HEADERS:");
         console.log(headers);
     });
 
-    parser.on("data", row => {
-        console.log("Первая строка получена");
-        process.exit(0);
+    parser.on("data", (row) => {
+
+        if (first) {
+            first = false;
+
+            console.log("ПЕРВАЯ СТРОКА:");
+            console.log(JSON.stringify(row, null, 2));
+
+            process.exit(0);
+        }
+
     });
 
-    parser.on("error", console.error);
+    parser.on("error", (err) => {
+        console.error("CSV ERROR:");
+        console.error(err);
+    });
+
+    res.on("error", (err) => {
+        console.error("HTTP ERROR:");
+        console.error(err);
+    });
 
     res.pipe(parser);
 
-}).on("error", console.error);
+}).on("error", (err) => {
+    console.error(err);
+});
